f7bf25167c78c28b7decbfe7d0bf415c01230ac0
psytek@alphaonelabs.com <psytek@alphaonelabs.com@4f7a2f2e-fcd3-11de-a138-4f08880ddf14>
2011-01-20 02:17:13 +0000

    Removing comment

--- a1l-rfid/trunk/Models.py
+++ a1l-rfid/trunk/Models.py
@@ -5,8 +5,6 @@ class RFIDMapping(db.Model):
     name = db.StringProperty()
     oauth_token = db.StringProperty()
     oauth_secret = db.StringProperty()
-    # For instance, we may want to reuse a tag and 
-    # keep track of the old name for the access log.
     status = db.StringProperty()
 
 class AccessLog(db.Model):
